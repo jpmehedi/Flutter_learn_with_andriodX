fdbc261a4f56392a8f386d50e6027d5e56ef14da
DESKTOP-0B563Q2\engrm <engrmhnirob47@gmail.com>
2020-01-14 06:54:06 +0600

    project upload

---
+++ flutterlearn/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Home page'),
+        ),
+        body: Center(
+          child: FlatButton(
+            onPressed: (){
+              
+              final player = AudioCache();
+              player.play('note1.wav');
+             
+            },
+            child: Text("Click Me"),
+          ),
+        ),
+      ),
+    );
+  }
+}
