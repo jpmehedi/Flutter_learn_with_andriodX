ec865ecf018493b55e7d0207cb9f54b14f7d419d
DESKTOP-0B563Q2\engrm <engrmhnirob47@gmail.com>
2020-01-14 07:34:40 +0600

    How to play multipale sounds

--- flutterlearn/lib/main.dart
+++ flutterlearn/lib/main.dart
@@ -4,7 +4,11 @@ import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  void soundFunc(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -12,16 +16,45 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Home page'),
         ),
-        body: Center(
-          child: FlatButton(
-            onPressed: (){
-              
-              final player = AudioCache();
-              player.play('note1.wav');
-             
-            },
-            child: Text("Click Me"),
-          ),
+        body: Column(
+          children: <Widget>[
+            FlatButton(
+              color: Colors.red,
+              onPressed: () {
+                soundFunc(1);
+              },
+            ),
+            FlatButton(
+              color: Colors.orange,
+              onPressed: () {
+                soundFunc(2);
+              },
+            ),
+            FlatButton(
+              color: Colors.green,
+              onPressed: () {
+                soundFunc(3);
+              },
+            ),
+            FlatButton(
+              color: Colors.yellow,
+              onPressed: () {
+                soundFunc(4);
+              },
+            ),
+            FlatButton(
+              color: Colors.blue,
+              onPressed: () {
+                soundFunc(5);
+              },
+            ),
+            FlatButton(
+              color: Colors.purple,
+              onPressed: () {
+                soundFunc(7);
+              },
+            ),
+          ],
         ),
       ),
     );
