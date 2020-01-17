80e32b72378187699e9e94c4ffa0e1f18c256d96
DESKTOP-0B563Q2\engrm <engrmhnirob47@gmail.com>
2020-01-17 12:21:22 +0600

    abstraction

---
+++ flutterlearn/lib/Question.dart
@@ -0,0 +1,12 @@
+class Question {
+
+  String questionText;
+  bool questionAns;
+
+  Question({a:String, b:bool}){
+
+    questionText = a;
+    questionAns = b;
+  }
+  
+}
--- flutterlearn/lib/main.dart
+++ flutterlearn/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'quiz_brain.dart';
+
+QuizBrain quizBrain = new QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -26,11 +29,6 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreCard = [];
-  List<String> question = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.',
-  ];
   int questionNumber = 0;
 
   @override
@@ -45,7 +43,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-               question [questionNumber],
+                quizBrain.questionBank[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -69,8 +67,16 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                bool correctAnswer = quizBrain.questionBank[questionNumber].questionAns;
+
+                if (correctAnswer == true) {
+                  print("Answer is correct!");
+                } else {
+                  print("Answer is wrong!");
+                }
+
                 setState(() {
-                 questionNumber++;
+                  questionNumber++;
                 });
                 //The user picked true.
               },
@@ -90,10 +96,15 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                bool correctAnswer = quizBrain.questionBank[questionNumber].questionAns;
+                if (correctAnswer == false) {
+                  print("Answer is correct!");
+                } else {
+                  print("Answer is wrong!");
+                }
                 setState(() {
-                    questionNumber++;
+                  questionNumber++;
                 });
-             
               },
             ),
           ),
